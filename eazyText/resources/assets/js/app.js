967463e36de4d33952ae3b11da98b175858c9a8d
Nicko <nicolasmathieu31@hotmail.com>
2017-02-22 14:29:05 +0100

    Recup

--- eazyText/resources/assets/js/app.js
+++ eazyText/resources/assets/js/app.js
@@ -5,38 +5,39 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-(function(){
- 	"use strict";
- 	var app = {
+ console.log('yooo!');
+// (function(){
+//  	"use strict";
+//  	var app = {
 
- 		init : function(){
- 			app.hideMenu();
+//  		init : function(){
+//  			app.hideMenu();
 
- 			// app.popUp();
- 		},
+//  			// app.popUp();
+//  		},
 
- 		hideMenu : function(){
- 			$(window).scroll(function (){
- 				var carous = $('#menu');
- 				if ($(this).scrollTop() > 700 ) {
- 					carous.hide();
+//  		hideMenu : function(){
+//  			$(window).scroll(function (){
+//  				var carous = $('#menu');
+//  				if ($(this).scrollTop() > 700 ) {
+//  					carous.hide();
  					
- 				}else{
- 					carous.show();
- 				};
- 			});
- 		},
+//  				}else{
+//  					carous.show();
+//  				};
+//  			});
+//  		},
  		
 
- 	};
- 	$(document).ready(function(){
- 		app.init();
- 	});
- })();
+//  	};
+//  	$(document).ready(function(){
+//  		app.init();
+//  	});
+//  })();
