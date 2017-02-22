fb18e6efd6f097dadbd96e7e33c4dbdae34f1779
Nicko <nicolasmathieu31@hotmail.com>
2017-02-22 14:33:46 +0100

    Maj

--- eazyText/resources/assets/js/app.js
+++ eazyText/resources/assets/js/app.js
@@ -1,2 +1,21 @@
+(function(){
+     "use strict";
+     var app = {
 
+        init : function(){
+             app.listener();
+         },
+
+        listener : function(){
+             $('#btn').on('click', function(){
+                 console.log('teste');
+             })
+         },
+         
+
+    };
+     $(document).ready(function(){
+         app.init();
+     });
+ })();
 
